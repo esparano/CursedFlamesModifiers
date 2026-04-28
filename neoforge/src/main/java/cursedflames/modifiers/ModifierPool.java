023f3150a470cec343ac955386c192bd8d8ac363
esparano <evansparano@gmail.com>
2026-04-27 23:45:19 -0400

    ModifierPool

---
+++ neoforge/src/main/java/cursedflames/modifiers/ModifierPool.java
@@ -0,0 +1,39 @@
+package cursedflames.modifiers;
+
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ModifierPool {
+  public Predicate<ItemStack> isApplicable;
+  public int totalWeight = 0;
+  //  // Pool weight, for choosing which pool to use when multiple are applicable.
+//  public int poolWeight = 0;
+  public List<Modifier> modifiers = new ArrayList<>();
+
+  public ModifierPool(Predicate<ItemStack> isApplicable) {
+    this.isApplicable = isApplicable;
+  }
+
+  public void add(Modifier mod) {
+    modifiers.add(mod);
+    totalWeight += mod.definition().weight();
+  }
+
+  public Modifier roll(RandomSource random) {
+    if (totalWeight == 0 || modifiers.isEmpty()) return null;
+    int i = random.nextInt(totalWeight);
+    int j = 0;
+    for (Modifier modifier : modifiers) {
+      j += modifier.definition().weight();
+      if (i < j) {
+        return modifier;
+      }
+    }
+    // This shouldn't happen
+    return null;
+  }
+}
